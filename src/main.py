0261799030487bf474c48900559ee85f771b8bb2
Paweł Kopaniecki <81909489+pawelkopaniecki@users.noreply.github.com>
2024-01-25 17:43:59 +0100

    fix: add ping endpoint

--- src/main.py
+++ src/main.py
@@ -6,6 +6,10 @@ from .schemas import EmailSchema
 app = FastAPI()
 
 
+@app.get("/api/v1/ping", status_code=200)
+async def email_route(email: EmailSchema):
+    return { "message": "Email service is running" }
+
 @app.post("/api/v1/email/send", status_code=201)
 async def email_route(email: EmailSchema):
     response = send_email(**email.__dict__)
